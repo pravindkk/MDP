343b0bacadc74c8f14c3a5f4198457e1128df604
kevintanyh <95677318+kevintanyh@users.noreply.github.com>
2024-03-04 10:20:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -34,46 +34,48 @@ def path_finding():
     robot_x, robot_y = content['robot_x'], content['robot_y']
     robot_direction = int(content['robot_dir'])
 
-    # Initialize MazeSolver object with robot size of 20x20, bottom left corner of robot at (1,1), facing north, and whether to use a big turn or not.
-    maze_solver = MazeSolver(20, 20, robot_x, robot_y, robot_direction, big_turn=None)
+    # # Initialize MazeSolver object with robot size of 20x20, bottom left corner of robot at (1,1), facing north, and whether to use a big turn or not.
+    # maze_solver = MazeSolver(20, 20, robot_x, robot_y, robot_direction, big_turn=None)
 
-    # Add each obstacle into the MazeSolver. Each obstacle is defined by its x,y positions, its direction, and its id
-    for ob in obstacles:
-        maze_solver.add_obstacle(ob['x'], ob['y'], ob['d'], ob['id'])
+    # # Add each obstacle into the MazeSolver. Each obstacle is defined by its x,y positions, its direction, and its id
+    # for ob in obstacles:
+    #     maze_solver.add_obstacle(ob['x'], ob['y'], ob['d'], ob['id'])
 
-    start = time.time()
-    # Get shortest path
-    optimal_path, distance = maze_solver.get_optimal_order_dp(retrying=retrying)
-    print(f"Time taken to find shortest path using A* search: {time.time() - start}s")
-    print(f"Distance to travel: {distance} units")
+    optimal_path = AlgoFunctions.NearestNeighbourSearch()
+
+    # start = time.time()
+    # # Get shortest path
+    # optimal_path, distance = maze_solver.get_optimal_order_dp(retrying=retrying)
+    # print(f"Time taken to find shortest path using A* search: {time.time() - start}s")
+    # print(f"Distance to travel: {distance} units")
     
-    # Based on the shortest path, generate commands for the robot
-    commands = command_generator(optimal_path, obstacles)
+    # # Based on the shortest path, generate commands for the robot
+    # commands = command_generator(optimal_path, obstacles)
 
-    # Get the starting location and add it to path_results
-    path_results = [optimal_path[0].get_dict()]
-    # Process each command individually and append the location the robot should be after executing that command to path_results
-    i = 0
-    for command in commands:
-        if command.startswith("SNAP"):
-            continue
-        if command.startswith("FIN"):
-            continue
-        elif command.startswith("FW") or command.startswith("FS"):
-            i += int(command[2:]) // 10
-        elif command.startswith("BW") or command.startswith("BS"):
-            i += int(command[2:]) // 10
-        else:
-            i += 1
-        path_results.append(optimal_path[i].get_dict())
-    return jsonify({
-        "data": {
-            'distance': distance,
-            'path': path_results,
-            'commands': commands
-        },
-        "error": None
-    })
+    # # Get the starting location and add it to path_results
+    # path_results = [optimal_path[0].get_dict()]
+    # # Process each command individually and append the location the robot should be after executing that command to path_results
+    # i = 0
+    # for command in commands:
+    #     if command.startswith("SNAP"):
+    #         continue
+    #     if command.startswith("FIN"):
+    #         continue
+    #     elif command.startswith("FW") or command.startswith("FS"):
+    #         i += int(command[2:]) // 10
+    #     elif command.startswith("BW") or command.startswith("BS"):
+    #         i += int(command[2:]) // 10
+    #     else:
+    #         i += 1
+    #     path_results.append(optimal_path[i].get_dict())
+    # return jsonify({
+    #     "data": {
+    #         'distance': distance,
+    #         'path': path_results,
+    #         'commands': commands
+    #     },
+    #     "error": None
+    # })
 
 
 @app.route('/image', methods=['POST'])
